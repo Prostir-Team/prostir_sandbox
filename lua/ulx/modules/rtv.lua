b646f79fca6bfed74e5ec5c1832422f3a819f858
Swanchick <61909965+Swanchick@users.noreply.github.com>
2023-09-23 23:00:49 +0300

    Update rtv.lua

--- lua/ulx/modules/rtv.lua
+++ lua/ulx/modules/rtv.lua
@@ -1,7 +1,19 @@
-function ulx.rtv(calling_ply)
+local PLAYER_VOTES = {}
 
+function ulx.rtv(calling_ply)
+    if not IsValid(calling_ply) then return end
+    if not calling_ply:IsPlayer() then return end
+
+    local steamid = calling_ply:SteamID()
+
+    if table.HasValue(PLAYER_VOTES, steamid) then
+        print(calling_ply:Nick() .. " has already voted.")
+        return
+    end
+
+   table.insert(PLAYER_VOTES, steamid) 
 end
 
 local rtv = ulx.command("Sandbox", "ulx rtv", ulx.rtv, "!rtv")
 rtv:defaultAccess(ULib.ACCESS_ALL)
-rtv::help("Голосування")
+rtv:help("Голосування")
