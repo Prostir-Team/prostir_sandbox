a61e1d3bd07a41967a3a4f32d55c4ef1849a310b
n1clude <56724583+n1clud3@users.noreply.github.com>
2023-11-03 17:48:35 +0200

    Покращене відображення грошей в худі
    @deltamolfar коли буде можливість, перероби це в своєму худі

--- lua/prsbox/money/cl_money.lua
+++ lua/prsbox/money/cl_money.lua
@@ -1,5 +1,12 @@
 local PLAYER_MONEY = 0
 
+surface.CreateFont("prsboxMoneyHUDLarge", {
+    font = "Roboto",
+    size = 30
+})
+
+local mat_dollar_icon = Material("icon16/money_dollar.png")
+
 net.Receive("PRSBOX.Net.OnMoneyGet", function(len, ply)
     local newMoney = net.ReadInt(21) -- New player money
 
@@ -14,5 +21,12 @@ function getLocalPlayerMoney()
 end
 
 hook.Add("HUDPaint", "TEST.Money.Hud", function ()
-    draw.DrawText(PLAYER_MONEY, "DermaLarge", 0, 0, COLOR_WHITE, TEXT_ALIGN_LEFT)
+    surface.SetFont("prsboxMoneyHUDLarge")
+    local pw, ph = surface.GetTextSize(PLAYER_MONEY)
+
+    draw.RoundedBox(4, ScrW() * 0.01, ScrH() * 0.02, 40 + pw, 36, Color(0,0,0,105))
+    surface.SetDrawColor(255,255,255,255)
+    surface.SetMaterial(mat_dollar_icon)
+    surface.DrawTexturedRect(ScrW() * 0.01 + 8, ScrH() * 0.02 + 8, 20, 20)
+    draw.DrawText(PLAYER_MONEY, "prsboxMoneyHUDLarge", ScrW() * 0.01 + 32, ScrH() * 0.02 + 3, COLOR_WHITE, TEXT_ALIGN_LEFT)
 end)
