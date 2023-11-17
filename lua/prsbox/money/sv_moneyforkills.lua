b03c932e279329b307390006313eedae7cac3335
n1clude <56724583+n1clud3@users.noreply.github.com>
2023-11-17 13:23:28 +0200

    Fix money error on death by falling

--- lua/prsbox/money/sv_moneyforkills.lua
+++ lua/prsbox/money/sv_moneyforkills.lua
@@ -31,7 +31,9 @@ end
 
 hook.Add("PlayerDeath", "PRSBOX.MoneyForKill", function(victim, inflictor, attacker)
     if (attacker == victim) then return end
-    if not (attacker:IsPlayer() or victim:IsPlayer() or inflictor:IsValid()) then return end
+    if not (attacker:IsPlayer()) then return end
+    if not (victim:IsPlayer()) then return end
+    if not (inflictor:IsValid()) then return end
     print(victim:GetClass(), inflictor:GetClass(), attacker:GetClass())
     local minMoneyValue = cfg.simpleKill.minMoney
     local maxMoneyValue = cfg.simpleKill.maxMoney
