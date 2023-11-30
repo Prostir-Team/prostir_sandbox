4ab072b51371acb5ee321a8fbd390ee1179e16ce
n1clude <56724583+n1clud3@users.noreply.github.com>
2023-11-30 09:55:32 +0200

    Money icon change + slight redesign

--- lua/prsbox/money/cl_money.lua
+++ lua/prsbox/money/cl_money.lua
@@ -1,11 +1,11 @@
-local PLAYER_MONEY = 0
+PLAYER_MONEY = 0
 
 surface.CreateFont("prsboxMoneyHUDLarge", {
     font = "Roboto",
-    size = 30
+    size = 40
 })
 
-local mat_dollar_icon = Material("icon16/money_dollar.png")
+local mat_dollar_icon = Material("hud/money2.png")
 
 net.Receive("PRSBOX.Net.OnMoneyGet", function(len, ply)
     local newMoney = net.ReadInt(21) -- New player money
@@ -22,13 +22,18 @@ end
 
 local function drawHud()
     surface.SetFont("prsboxMoneyHUDLarge")
-    local pw, ph = surface.GetTextSize(PLAYER_MONEY)
+    --local pw, ph = surface.GetTextSize(PLAYER_MONEY)
+    --print(pw)
 
-    draw.RoundedBox(4, ScrW() * 0.01, ScrH() * 0.02, 40 + pw, 36, Color(0,0,0,105))
+    local posx = ScrW() * 0.885
+    local posy = ScrH() * 0.02
+
+    draw.RoundedBox(4, posx, posy, 48 + 118, 48, Color(0,0,0,105))
     surface.SetDrawColor(255,255,255,255)
     surface.SetMaterial(mat_dollar_icon)
-    surface.DrawTexturedRect(ScrW() * 0.01 + 8, ScrH() * 0.02 + 8, 20, 20)
-    draw.DrawText(PLAYER_MONEY, "prsboxMoneyHUDLarge", ScrW() * 0.01 + 32, ScrH() * 0.02 + 3, COLOR_WHITE, TEXT_ALIGN_LEFT)
+    surface.DrawTexturedRect(posx + 8, posy + 9, 32, 32)
+    --draw.DrawText("€$", "eurodollarIcon", posx + 8, posy + 9, COLOR_WHITE, TEXT_ALIGN_LEFT)
+    draw.DrawText(PLAYER_MONEY, "prsboxMoneyHUDLarge", posx + 158, posy + 4, COLOR_WHITE, TEXT_ALIGN_RIGHT)
 end
 
 local hudvar = GetConVar("cl_drawhud"):GetBool() --По хорошому треба окремий хук, який не викликається без цього квара
--- lua/prsbox/money/cl_spawnmenu.lua
+++ lua/prsbox/money/cl_spawnmenu.lua
@@ -5,7 +5,7 @@ surface.CreateFont("prsboxMoneyHUDMedium", {
     size = 20
 })
 
-local mat_dollar_icon = Material("icon16/money_dollar.png")
+local mat_dollar_icon = Material("hud/money2.png")
 
 net.Receive("PRSBOX.Net.SendPrices", function (len)
     LOCAL_PRICE = net.ReadTable()
@@ -19,11 +19,25 @@ hook.Add("PRSBOX.ContentIcon.Paint", "PRSBOX.ShowPrices", function (panel, w, h)
     local price = tostring(LOCAL_PRICE[classname])
 
     surface.SetFont("prsboxMoneyHUDMedium")
-    local pw, ph = surface.GetTextSize(price)
+    --local pw, ph = surface.GetTextSize(price)
+
+    local iconcolor = Color(255, 255, 255)
+    local textcolor = Color(255, 255, 255)
+    if tonumber(PLAYER_MONEY) > tonumber(price) then
+        iconcolor = Color(100, 255, 100)
+        textcolor = Color(60, 255, 60)
+    else
+        iconcolor = Color(255, 100, 100)
+        textcolor = Color(255, 60, 60)
+    end
     
-    draw.RoundedBox(4, 7, 7, 32 + pw, 28, Color(0,0,0,220))
-    surface.SetDrawColor(255,255,255,255)
+    --draw.RoundedBox(4, 7, 7, 32 + pw, 28, Color(0,0,0,120))
+    surface.SetDrawColor(0, 0, 0)
     surface.SetMaterial(mat_dollar_icon)
-    surface.DrawTexturedRect(10, 10, 20, 20)
-    draw.DrawText(price, "prsboxMoneyHUDMedium", 31, 11, COLOR_WHITE, TEXT_ALIGN_LEFT)
+    surface.DrawTexturedRect(11, 13, 20, 20)
+    draw.DrawText(price, "prsboxMoneyHUDMedium", 34, 12, COLOR_BLACK, TEXT_ALIGN_LEFT)
+    surface.SetDrawColor(iconcolor)
+    surface.SetMaterial(mat_dollar_icon)
+    surface.DrawTexturedRect(10, 12, 20, 20)
+    draw.DrawText(price, "prsboxMoneyHUDMedium", 33, 11, textcolor, TEXT_ALIGN_LEFT)
 end)
