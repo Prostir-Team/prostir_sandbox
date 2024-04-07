705c7a7b23a1750e1445f24dbb89b1426943fcef
n1clude <56724583+n1clud3@users.noreply.github.com>
2024-04-07 12:16:25 +0300

    Getting ready to open the sources

--- lua/prsbox/relay/sv_config.lua
+++ lua/prsbox/relay/sv_config.lua
@@ -1,11 +1,11 @@
 Discord = {
-	['webhook'] = "https://discord.com/api/webhooks/1019508656577990706/lL-bgwRoBShjJ7mLLozh84Fhr6bSOR9M-vu47SYVO2UDO6_a5GJLa0XqkjrjkzDab2mp",
+	['webhook'] = "https://discord.com/api/webhooks/1019508656577990706/lL-bgwRoBShjJ7mLLozh84Fhr6bSOR9M-vu47SYVO2UDO6_a5GJLa0XqkjrjkzDab2mp", -- not valid anymore :D
 	
 	['hookname'] = "Бандеро відправляч",
 
 	['readChannelID'] = "1019508554962587689",
 
-	['botToken'] = 'ODYxNzEzNzQ5NTQ2NjMxMjA4.GScE7n.YVeEA8yY8tF2jq5x4FYgZq7iZ4E6zG0cnaZ_BI',
+	['botToken'] = 'ODYxNzEzNzQ5NTQ2NjMxMjA4.GScE7n.YVeEA8yY8tF2jq5x4FYgZq7iZ4E6zG0cnaZ_BI', -- not valid anymore :D
 
 	["botPrefix"] = "!",
 
